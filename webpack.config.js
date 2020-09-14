9615e63f18beeead62df725fc60d7642bf585743
Almerindo Paixão <almerindopaixao@gmail.com>
2020-09-14 20:39:31 -0300

    estruturação do projeto

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.ts',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/,
+        options: {
+          configFile: 'tsconfig.json',
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public', 'assets', 'js'),
+  },
+  devtool: 'source-map',
+};
